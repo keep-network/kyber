e5138fc12a97b9777c1b1f04f1100140817b6dce
nikkolasg <nicolas.gailly@epfl.ch>
2017-07-13 15:12:23 +0200

    changing setup path to global golang src

--- group/pbc/setup.sh
+++ group/pbc/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 dfinityPath="$HOME/prog/dfinity/crypto"
-dedisPath="$GOPATH/src/gopkg.in"
+dedisPath="$GOPATH/src"
 docker rm -f dfinity
-docker run --rm -it --name dfinity -v $dfinityPath:/workspace -v $dedisPath:/workspace/go/src/gopkg.in dfinity/build 
+docker run --rm -it --name dfinity -v $dfinityPath:/workspace -v $dedisPath:/workspace/go/src dfinity/build 
